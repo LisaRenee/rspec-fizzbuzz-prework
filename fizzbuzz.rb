39c6e59038b8a7b48ac4161ba457846e8c66fe18
Lisa Scott <lrscot@aol.com>
2019-02-03 00:50:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 #Fizz(15) returns "FizzBuzz"
 
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 
+  if int % 3 == 0 && int % 5 == 0
     "FizzBuzz" # Go fizz
   elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz"
